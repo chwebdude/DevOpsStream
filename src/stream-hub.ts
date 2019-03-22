161c1f645c433690370096bdd6343d172025f5b3
fabrice.andreis@gmail.com <7746357+chwebdude@users.noreply.github.com>
2019-03-22 14:35:41 +0100

    print builds into console

--- src/stream-hub.ts
+++ src/stream-hub.ts
@@ -1,9 +1,20 @@
+import RestClient = require("TFS/Build/RestClient");
+
+var client = RestClient.getClient();
+client.getBuilds()
+    .then(b => {
+        console.log(b);
+        b.forEach(element => {
+            console.log(element.definition.name);
+        });
+    });
 
 
-VSS.require(["VSS/Service", "TFS/WorkItemTracking/RestClient"], (service: any, api: any) => {
-    console.log("service", service);
-    console.log("api", api);
 
-    var witClient = service.getCollectionClient(api.WorkItemTrackingHttpClient);
-    console.log(witClient);
-});
+// VSS.require(["VSS/Service", "TFS/WorkItemTracking/RestClient"], (service: any, api: any) => {
+//     console.log("service", service);
+//     console.log("api", api);
+// VSS.getAccessToken().then(o => {console.log(o)});
+//     var witClient = service.getCollectionClient(api.WorkItemTrackingHttpClient);
+//     console.log(witClient);
+// });
