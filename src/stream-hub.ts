596cf4b1b218c2ff7bb9cdc800d332c4933b8e23
fabrice.andreis@gmail.com <7746357+chwebdude@users.noreply.github.com>
2019-03-22 13:40:22 +0100

    test client

--- src/stream-hub.ts
+++ src/stream-hub.ts
@@ -1,1 +1,9 @@
-console.log("test");
+
+
+VSS.require(["VSS/Service", "TFS/WorkItemTracking/RestClient"], (service: any, api: any) => {
+    console.log("service", service);
+    console.log("api", api);
+
+    var witClient = service.getCollectionClient(api.WorkItemTrackingHttpClient);
+    console.log(witClient);
+});
