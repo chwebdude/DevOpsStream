bbd888e7356c94969450a95a6f03693841eae300
fabrice.andreis@gmail.com <7746357+chwebdude@users.noreply.github.com>
2019-03-22 14:54:28 +0100

    include projectId

--- src/stream-hub.ts
+++ src/stream-hub.ts
@@ -1,7 +1,9 @@
 import RestClient = require("TFS/Build/RestClient");
 
+var projectId = VSS.getWebContext().project.id;
+
 var client = RestClient.getClient();
-client.getBuilds()
+client.getBuilds(projectId)
     .then(b => {
         console.log(b);
         b.forEach(element => {
@@ -10,11 +12,10 @@ client.getBuilds()
     });
 
 
-
-// VSS.require(["VSS/Service", "TFS/WorkItemTracking/RestClient"], (service: any, api: any) => {
+// VSS.require(["VSS/Service", "TFS/Build/RestClient"], (service: any, api: any) => {
 //     console.log("service", service);
 //     console.log("api", api);
-// VSS.getAccessToken().then(o => {console.log(o)});
+//     // VSS.getAccessToken().then(o => {console.log(o)});
 //     var witClient = service.getCollectionClient(api.WorkItemTrackingHttpClient);
 //     console.log(witClient);
 // });
