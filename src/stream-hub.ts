8889a013c500a4398d7253ef2e85bdd0e89c24b4
fabrice.andreis@gmail.com <7746357+chwebdude@users.noreply.github.com>
2019-03-22 15:56:38 +0100

    async load data and generate list

--- src/stream-hub.ts
+++ src/stream-hub.ts
@@ -1,24 +1,36 @@
+/// <reference path="element.ts"/>
 import RestClient = require("TFS/Build/RestClient");
 import Mustache = require("mustache");
+import { Element } from "./element";
 
 var projectId = VSS.getWebContext().project.id;
 
 
+async function getBuilds() {
+     var res: Element[] = [];
+    var client = RestClient.getClient();
+    try {
 
-
-var client = RestClient.getClient();
-client.getBuilds(projectId)
-    .then(b => {        
-        var builds = "";
-        $.get('templates/build.html', (buildTemplate: string) => {
-            b.forEach(element => {
-                builds += Mustache.render(buildTemplate, element);
-            });
-            $("#target").html(builds);
+        var builds: any[] = await client.getBuilds(projectId);
+        console.log("builds", builds);
+        
+        var template: string = await $.get('templates/build.html');
+        console.log("template", template);
+        builds.forEach(element => {
+            var html = Mustache.render(template, element);
+            res.push(new Element(html, element.lastChangedDate));
         });
-    });
+
+    } catch (error) {
+        console.error(error);
+    }
+
+    console.log(res);
+    return res;
+}
 
 
+getBuilds();
 // VSS.require(["VSS/Service", "TFS/Build/RestClient"], (service: any, api: any) => {
 //     console.log("service", service);
 //     console.log("api", api);
