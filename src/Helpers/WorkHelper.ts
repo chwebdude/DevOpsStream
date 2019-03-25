edd6d1f628966d576a20a90dcee2bfeb54e448c5
fabrice.andreis@gmail.com <7746357+chwebdude@users.noreply.github.com>
2019-03-25 15:50:46 +0100

    refactor

--- src/Helpers/WorkHelper.ts
+++ src/Helpers/WorkHelper.ts
@@ -10,42 +10,41 @@ export class WorkHelper implements IHelper {
         var res: Element[] = [];
         var client = RestClientWit.getClient();
         try {
-            await client.readReportingRevisionsGet(projectId, undefined, undefined, undefined, undefined // todo: daterange,
-                , undefined, true, false, true, ReportingRevisionsExpand.None, undefined, 200)
-                .then(async (wi) => {
-                    console.log("work items", wi);
-                    for (const w of wi.values) {
-                        if (w.fields["System.IsDeleted"]) {
-                            console.log("Is deleted", w);
+            var wi = await client.readReportingRevisionsGet(projectId, undefined, undefined, undefined, undefined // todo: daterange,
+                , undefined, true, false, true, ReportingRevisionsExpand.None, undefined, 200);
+
+            console.log("work items", wi);
+            for (const w of wi.values) {
+                if (w.fields["System.IsDeleted"]) {
+                    console.log("Is deleted", w);
+                }
+                else {
+                    var updates = await client.getUpdates(w.id);
+                    // console.log("updates", updates);
+                    updates.forEach(u => {
+                        var el: Element = {
+                            action: "",
+                            additionalInfo: "wi id:" + u.workItemId + "- rev: " + u.rev,
+                            date: u.revisedDate,
+                            user: u.revisedBy.displayName,
+                            imageUrl: u.revisedBy.imageUrl
+                        };
+                        if (u.rev == 1) {
+                            // First revision -> new WorkItem
+                            el.action = "New Workitem";
                         }
                         else {
-                            var updates = await client.getUpdates(w.id);
-                            // console.log("updates", updates);
-                            updates.forEach(u => {
-                                var el: Element = {
-                                    action: "",
-                                    additionalInfo: "wi id:" + u.workItemId + "- rev: " + u.rev,
-                                    date: u.revisedDate,
-                                    user: u.revisedBy.displayName,
-                                    imageUrl: u.revisedBy.imageUrl
-                                };
-                                if (u.rev == 1) {
-                                    // First revision -> new WorkItem
-                                    el.action = "New Workitem";
-                                }
-                                else {
-                                    // Work item updated
-                                    el.action = "Workitem updated";
-                                }
-                                // Todo: Check this for linked items
-                                if (el.date.getFullYear() == 9999 && u.fields["System.ChangedDate"].newValue != undefined) {
-                                    el.date = new Date(u.fields["System.ChangedDate"].newValue);
-                                }
-                                res.push(el);
-                            });
+                            // Work item updated
+                            el.action = "Workitem updated";
                         }
-                    }
-                });
+                        // Todo: Check this for linked items
+                        if (el.date.getFullYear() == 9999 && u.fields["System.ChangedDate"].newValue != undefined) {
+                            el.date = new Date(u.fields["System.ChangedDate"].newValue);
+                        }
+                        res.push(el);
+                    });
+                }
+            }
         }
         catch (error) {
             console.log(error);
