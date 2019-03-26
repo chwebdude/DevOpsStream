39a8deb7f6ac7df754e80c1fa469c78df51f4b89
fabrice.andreis@gmail.com <7746357+chwebdude@users.noreply.github.com>
2019-03-26 15:14:23 +0100

    Show some change info

--- src/Helpers/WorkHelper.ts
+++ src/Helpers/WorkHelper.ts
@@ -3,8 +3,6 @@ import RestClientWit = require("TFS/WorkItemTracking/RestClient");
 import { ReportingRevisionsExpand } from "TFS/WorkItemTracking/Contracts";
 import { Element } from "../element";
 import Mustache from "mustache";
-import { VssConnection } from "VSS/Service";
-import { ReviewResourceType } from "VSS/Gallery/Contracts";
 
 var projectId = VSS.getWebContext().project.id;
 
@@ -24,29 +22,62 @@ export class WorkHelper implements IHelper {
                 else {
                     var updates = await client.getUpdates(rev.id);
                     updates.forEach(u => {
-                        var el: Element = {
+                        var renderInfo = {
+                            id: rev.id,
+                            title: rev.fields["System.Title"],
+                            link: VSS.getWebContext().host.uri + "/_workitems/edit/" + rev.id,
                             action: "",
-                            additionalInfo: Mustache.render(template, {
-                                id: rev.id,
-                                title: rev.fields["System.Title"],
-                                link: VSS.getWebContext().host.uri + "/_workitems/edit/" + rev.id
-                            }),
+                            date: new Date(),
+                            updated: "",
+                            assignedTo: "",
+                            oldState: "",
+                            newState: ""
+                        }
+
+
+                        // 
+                        if (u.rev == 1) {
+                            // First revision -> new WorkItem
+                            renderInfo.action = "New Workitem";
+                        }
+                        else {
+                            // Work item updated
+                            renderInfo.action = "Workitem updated";
+
+                            if (u.fields["System.Description"]) {
+                                renderInfo.updated = "Description"
+                            }
+
+                            if (u.fields["System.AssignedTo"]) {
+                                if (u.fields["AssignedTo"].newValue["displayName"]) {
+                                    renderInfo.assignedTo = u.fields["AssignedTo"].newValue["displayName"];
+                                } else {
+                                    renderInfo.assignedTo = "nobody"; // unassign
+                                }
+                            }
+
+                            if (u.fields["System.State"]) {
+                                renderInfo.oldState = u.fields["System.State"].oldValue;
+                                renderInfo.newState = u.fields["System.State"].newValue;
+                            }
+
+                        }
+
+
+
+                        // Todo: Check this for linked items
+                        if (u.revisedDate.getFullYear() == 9999 && u.fields["System.ChangedDate"].newValue != undefined) {
+                            renderInfo.date = new Date(u.fields["System.ChangedDate"].newValue);
+                        }
+
+                        // Render and build element
+                        var el: Element = {
+                            action: renderInfo.action,
+                            additionalInfo: Mustache.render(template, renderInfo),
                             date: u.revisedDate,
                             user: u.revisedBy.displayName,
                             imageUrl: u.revisedBy.imageUrl
                         };
-                        if (u.rev == 1) {
-                            // First revision -> new WorkItem
-                            el.action = "New Workitem";
-                        }
-                        else {
-                            // Work item updated
-                            el.action = "Workitem updated";
-                        }
-                        // Todo: Check this for linked items
-                        if (el.date.getFullYear() == 9999 && u.fields["System.ChangedDate"].newValue != undefined) {
-                            el.date = new Date(u.fields["System.ChangedDate"].newValue);
-                        }
                         res.push(el);
                     });
                 }
